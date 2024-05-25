491cb25505797af34e9f079c74c881aa37e09203
F.Kanayama <fkanayam@redhat.com>
2024-05-25 03:21:05 +0000

    Update devcontainer for apple silicon and podman

---
+++ .devcontainer/postCreateContainer.sh
@@ -0,0 +1,21 @@
+apt-get update
+apt-get install -y make git nano bash curl wget
+echo 'source /usr/share/bash-completion/completions/git' >> ~/.bashrc
+
+cd /usr/share/bash-completion/completions
+curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+curl -O https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+chmod a+x git*.*
+ls -l $PWD/git*.* | awk '{print "source "$9}' >> ~/.bashrc
+
+echo 'GIT_PS1_SHOWDIRTYSTATE=true' >> ~/.bashrc
+echo 'GIT_PS1_SHOWUNTRACKEDFILES=true' >> ~/.bashrc
+echo 'GIT_PS1_SHOWUPSTREAM=true' >> ~/.bashrc
+
+# For Git SSH
+cp /mnt/hostosuser_homedir/.ssh/id_rsa /root/.ssh/
+
+echo 'alias gti=git' >> ~/.bashrc
+
+cd ~
+source ~/.bashrc
